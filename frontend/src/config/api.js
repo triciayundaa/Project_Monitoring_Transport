973be7c8e0bc62af335ba4507e5adddc79c41202
OliviaDelisa <oliviadelisa123@gmail.com>
2026-02-12 08:34:16 +0700

    yuhuy

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,7 +1,7 @@
 // src/config/api.js
 
 // Saat masih di laptop (local), pakai ini:
-const API_BASE_URL = 'http://10.89.12.64:3000';
+const API_BASE_URL = 'http://10.87.192.64:3000';
 
 // Nanti kalau sudah hosting, tinggal ganti baris di atas jadi alamat webnya.
 // Contoh: const API_BASE_URL = 'https://api-monitoring.railway.app';
